c4f942ac66710343f5740635cdae70c2edf476ae
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-10-31 21:09:00 +0200

    Atualizações

---
+++ api/aparelhos.php
@@ -0,0 +1,14 @@
+<?php
+  
+    include("{$_SERVER['DOCUMENT_ROOT']}/dawar/painel/lib/includes.php");
+    $con = AppConnect('dawar');
+    $conApi = AppConnect('information_schema');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    
+
+    $query = "INSERT INTO  aparelhos set unicoId = '{$_POST['id']}', nome = '{$_POST['nome']}', telefone = '{$_POST['telefone']}'";
+    $result = mysqli_query($con, $query);
+
+
+    echo json_encode(['status' => 'Success']);
