a557a961e60c3442e92e8898109f9a4c4e873ff7
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-10-31 18:11:04 +0200

    Atualizações

--- api/votar.php
+++ api/votar.php
@@ -1,9 +1,10 @@
 <?php
   
-  include("{$_SERVER['DOCUMENT_ROOT']}/dawar/painel/lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/dawar/painel/lib/includes.php");
     $con = AppConnect('dawar');
     $conApi = AppConnect('information_schema');
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
     
 
     $query = "INSERT INTO  votos set produto = '{$_POST['cod']}', tipo = '{$_POST['tipo']}', id = '{$_POST['uid']}', data = NOW()";
